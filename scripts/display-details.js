3784e0707274bf9ea2d81db6d8e0eca9a527de1f
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-04-25 22:38:18 +0200

    Details page update

--- scripts/display-details.js
+++ scripts/display-details.js
@@ -3,15 +3,17 @@ const countryDetails = JSON.parse(localStorage.getItem('countryDetails'));
 
 // Sélectionner l'élément où afficher les informations du pays
 const countryDetailsContainer = document.getElementById('country-details-container');
+const countryFlagContainer = document.querySelector('.country-flag-container');
+const countryDetailsInformation = document.querySelector('#country-details-informations')
 
 // Créer un élément <img> pour afficher le drapeau
 const flagImg = document.createElement('img');
 flagImg.src = countryDetails[0].flags.svg; // Utilisez l'URL du drapeau du pays depuis les données récupérées
 flagImg.alt = `${countryDetails[0].name.common} flag`;
-flagImg.classList.add('h-2/3', 'w-1/3', 'mr-4'); // Ajouter des classes pour le dimensionnement et la marge droite
+flagImg.classList.add('h-2/3', 'w-auto'); // Ajouter des classes pour le dimensionnement et la marge droite
 
 // Ajouter l'élément <img> au début du conteneur des détails du pays
-countryDetailsContainer.appendChild(flagImg);
+countryFlagContainer.appendChild(flagImg);
 
 // Créer un élément div pour les informations du pays
 const detailsInformationDiv = document.createElement('div');
@@ -22,11 +24,10 @@ countryDetailsContainer.appendChild(detailsInformationDiv);
 
 // Créer des éléments <p> pour chaque propriété spécifiée
 const properties = [
-    { label: 'Second Alt Spellings', key: 'altSpellings', alt: 'AltSpellings' },
-    { label: 'Native Name', key: 'nativeName', alt: 'Native name' },
+    { label: 'Native Name', key: 'altSpellings', alt: 'AltSpellings' },
     { label: 'Population', key: 'population' },
     { label: 'Region', key: 'region' },
-    { label: 'Subregion', key: 'subregion' },
+    { label: 'Sub Region', key: 'subregion' },
     { label: 'Capital', key: 'capital' },
     { label: 'Top Level Domain', key: 'tld' },
     { label: 'Currencies', key: 'currencies' },
@@ -34,8 +35,15 @@ const properties = [
     { label: 'Border Countries', key: 'borders' }
 ];
 
+const countryName = countryDetails[0].name.common;
+const countryHeader = document.createElement('h2');
+countryHeader.textContent = countryName;
+countryHeader.classList.add('font-bold', 'text', 'text-4xl', 'mb-4');
+countryDetailsInformation.appendChild(countryHeader);
+
 properties.forEach(property => {
     const propertyParagraph = document.createElement('p');
+    propertyParagraph.classList.add('text', 'text-md');
     const propertyValue = countryDetails[0][property.key];
 
     // Si la valeur de la propriété est un tableau, convertissez-le en une chaîne de caractères séparée par des virgules
@@ -47,5 +55,5 @@ properties.forEach(property => {
     propertyParagraph.textContent = text;
 
     // Ajouter l'élément <p> au conteneur des informations du pays
-    detailsInformationDiv.appendChild(propertyParagraph);
+    countryDetailsInformation.appendChild(propertyParagraph);
 });
